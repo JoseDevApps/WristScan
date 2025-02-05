a4c19659252852ae07714a9e513efe99f3c95ed0
Ibanez Quiroz <jose.ibanezquiroz@giz.de>
2025-02-05 13:46:41 -0400

    add: stripe component

--- App/dashboard/urls.py
+++ App/dashboard/urls.py
@@ -6,8 +6,11 @@ urlpatterns = [
     path('', views.inicio, name="inicio"),
     path('qrgen', views.qrgen, name="qrgen"),
     path('qrscan', views.qrscan, name="qrscan"),
-    path('create', views.assign, name="create"),
+    path('create', views.create, name="create"),
     path('assign', views.basic, name="assign"),
     path('basic', views.tables, name="basic"),
     path('tables', views.qrgen, name="tables"),
+    path('create-checkout-session/<int:pk>/', views.create_checkout_session, name='create_checkout_session'),
+    path('cancel/', views.CancelView.as_view(), name='cancel'),
+    path('success/', views.SuccessView.as_view(), name='success'),
 ]
--- App/dashboard/views.py
+++ App/dashboard/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.conf import settings
+from django.http import JsonResponse
+from django.shortcuts import redirect
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
+import stripe
+from django.views.generic import TemplateView
+from .models import Product
 
 ################################################
 #   Pagina de bienvenida
@@ -28,7 +36,17 @@ def qrscan(request):
 ################################################
 def create(request):
     template = 'dashboard/create.html' 
-    context = {}
+    product1 = Product.objects.get(name="Plan 1")
+    product2 = Product.objects.get(name="Plan 2")
+    product3 = Product.objects.get(name="Plan 3")
+    print(product1.id)
+
+    context = {
+        "product1": product1,
+        "product2": product2,
+        "product3": product3, 
+        "STRIPE_PUBLIC_KEY": settings.STRIPE_PUBLIC_KEY
+        }
     return render(request, template, context)
 ################################################
 #   Pagina de QR assign assign
@@ -51,3 +69,52 @@ def tables(request):
     template = 'dashboard/tables.html' 
     context = {}
     return render(request, template, context)
+
+################################################
+#   Pagina de pagos
+################################################
+@csrf_exempt
+def create_checkout_session(request, pk):
+    if request.method == "POST":
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        print('checkout session init')
+        product = get_object_or_404(Product, id=pk)
+        YOUR_DOMAIN = "http://127.0.0.1"
+        
+        checkout_session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[
+                {
+                    'price_data': {
+                        'currency': 'usd',
+                        'unit_amount': product.price,
+                        'product_data': {
+                            'name': product.name,
+                        },
+                    },
+                    'quantity': 1,
+                },
+            ],
+            metadata={
+                "product_id": product.id
+            },
+            mode='payment',
+            # Modificar
+            success_url=YOUR_DOMAIN + '/dashboard/success',
+            cancel_url=YOUR_DOMAIN + '/dashboard/cancel',
+        )
+        
+        return redirect(checkout_session.url, code=303)
+    return JsonResponse({'error': 'Invalid request method'}, status=400)
+################################################
+#   Pagina de pagos ok
+################################################
+
+class SuccessView(TemplateView):
+    template_name = "dashboard/success.html"
+
+################################################
+#   Pagina de pagos cancel
+################################################
+class CancelView(TemplateView):
+    template_name = "dashboard/cancel.html"
