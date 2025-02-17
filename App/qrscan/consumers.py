d360ede138f23c96046c7e4b13f7c4d99f36b791
Ibanez Quiroz <jose.ibanezquiroz@giz.de>
2025-02-17 12:34:41 -0400

    updated: models

--- App/qrscan/consumers.py
+++ App/qrscan/consumers.py
@@ -24,14 +24,14 @@ class QRConsumer(AsyncWebsocketConsumer):
         if qr_code:
             # Check if the QR code already exists in the database
             existing_qr = await self.query_qr_code(qr_code)
-            if len(existing_qr) > 0:
-                if existing_qr is not None:
-                    if existing_qr[7]=='nuevo':
-                    # If QR code exists, send a response back that it's already processed
-                        response_message = f'QR - {existing_qr[0]} ingreso concedido'
-                    if existing_qr[7]=='concedido':
-                        date = existing_qr[8].astimezone(timezone(timedelta(hours=-4))).strftime('%Y-%m-%d %H:%M:%S')
-                        response_message = f'QR - {existing_qr[0]} - {date}'
+
+            if existing_qr is not None:
+                if existing_qr[7]=='nuevo':
+                # If QR code exists, send a response back that it's already processed
+                    response_message = f'QR - {existing_qr[0]} ingreso concedido'
+                if existing_qr[7]=='concedido':
+                    date = existing_qr[8].astimezone(timezone(timedelta(hours=-4))).strftime('%Y-%m-%d %H:%M:%S')
+                    response_message = f'QR - {existing_qr[0]} - {date}'
 
             else:
                 response_message = "The QR code you scanned is invalid or does not exist."
