02dc90f02726e8a75a585a66a9f7d2585ffdf432
Ibanez Quiroz <jose.ibanezquiroz@giz.de>
2025-02-20 11:38:23 -0400

    update: share codes

--- App/dashboard/views.py
+++ App/dashboard/views.py
@@ -81,10 +81,10 @@ def share_qr_codes(request):
                         qr_code.status_purchased = 'purchased'
                         qr_code.user_email = recipient_email
                         qr_code.save()
-                        qr_code_path = os.path.join(settings.MEDIA_ROOT, 'qrcodes/', f"{event.id}-{qr_code.data}_final.png")
+                        qr_code_path = os.path.join(settings.MEDIA_ROOT, 'qrcodes/', f"{qr_code.data}_final.png")
                         print(qr_code_path)
                         if os.path.exists(qr_code_path):
-                            zip_file.write(qr_code_path, f"{event.id}-{qr_code.data}_final.png")
+                            zip_file.write(qr_code_path, f"{qr_code.data}_final.png")
                         codes_shared.append(qr_code.data)
                 zip_buffer.seek(0)
                 # Send email to the recipient
