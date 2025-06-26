5489886a7cc59d535cbb244930d6defe10097f6c
Ibanez Quiroz <jose.ibanezquiroz@giz.de>
2025-06-26 04:42:30 -0400

    update: DB

--- App/qrcodes/admin.py
+++ App/qrcodes/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Event, QRCode, EventRole
+from .models import Event, QRCode, EventRole, PriceTier, Ticket, Payment, TicketAssignment
 
+admin.site.register(PriceTier)
+admin.site.register(Ticket)
+admin.site.register(Payment)
 admin.site.register(Event)
 admin.site.register(QRCode)
 admin.site.register(EventRole)
+admin.site.register(TicketAssignment)
