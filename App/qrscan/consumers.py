2f702da0f3a00a7dbaa2018d5e3c7c377325766b
Ibanez Quiroz <jose.ibanezquiroz@giz.de>
2025-07-01 11:59:29 -0400

    update:database

--- App/qrscan/consumers.py
+++ App/qrscan/consumers.py
@@ -17,13 +17,13 @@ class QRConsumer(AsyncWebsocketConsumer):
         # Receive QR code from the WebSocket
         text_data_json = json.loads(text_data)
         qr_code = text_data_json.get('qr_code')
-        eventid = text_data_json.get('eventid')
+        # eventid = text_data_json.get('eventid')
         # Process the QR code (e.g., validate it)
         # You can also interact with the database or perform any task here.
         print(qr_code['decodedText'])
         if qr_code:
             # Check if the QR code already exists in the database
-            existing_qr = await self.query_qr_code(qr_code, eventid)
+            existing_qr = await self.query_qr_code(qr_code)
 
             if existing_qr is not None:
                 if existing_qr[7]=='nuevo':
@@ -45,7 +45,7 @@ class QRConsumer(AsyncWebsocketConsumer):
             'message': response_message
         }))
     @sync_to_async
-    def query_qr_code(self, qr_code, eventid):
+    def query_qr_code(self, qr_code):
         with connection.cursor() as cursor:
             # Example raw SQL query
             cursor.execute("SELECT * FROM qrcodes_qrcode WHERE data = %s", [qr_code['decodedText']])
