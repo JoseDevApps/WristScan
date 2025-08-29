a92673e2c32d8663e412691b6b1b07aac927c4b8
Ibanez Quiroz <jose.ibanezquiroz@giz.de>
2025-08-29 06:52:26 -0400

    update geoip

--- App/manillas/settings.py
+++ App/manillas/settings.py
@@ -69,7 +69,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "manillasbolivia.middleware.geoip_simple.SimpleGeoIPMiddleware",
+    "manillas.middleware.geoip_simple.SimpleGeoIPMiddleware",
  ]
 
 ROOT_URLCONF = "manillas.urls"
