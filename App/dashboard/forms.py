72080242f1ba723f39e166163ee4fa18dde3b768
Ibanez Quiroz <jose.ibanezquiroz@giz.de>
2025-10-10 12:41:22 -0400

    update table

--- App/dashboard/forms.py
+++ App/dashboard/forms.py
@@ -87,7 +87,6 @@ class EventSelectorForm(forms.Form):
         queryset=Event.objects.none(),  # inicial vacío
         widget=forms.Select(attrs={
             'class': 'form-control mx-auto',  # 'mx-auto' centra horizontalmente
-            'style': 'display:block; width:50%;'  # opcional: ancho al 50%
         }),
         label="Select Event",
         empty_label="Choose one of your events...",
