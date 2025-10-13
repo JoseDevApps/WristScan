cfda871102321ec7f5a04b3bed612bbbccb5a544
Ibanez Quiroz <jose.ibanezquiroz@giz.de>
2025-10-13 10:03:01 -0400

    update table

--- App/manillas/settings.py
+++ App/manillas/settings.py
@@ -163,7 +163,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = "es"
+LANGUAGE_CODE = "en"
 
 TIME_ZONE = "America/La_Paz"
 
